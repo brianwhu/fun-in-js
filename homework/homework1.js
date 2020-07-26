c2ba350bcd40e2938def12b9f503d0d626bcf0c5
Brian Hu <brianwhu@gmail.com>
2020-07-26 16:27:32 -0400

    Homework 1 assignment

---
+++ homework/homework1.js
@@ -0,0 +1,32 @@
+let family = [
+    {
+        name: "Brian",
+        location: "Florida",
+        isAdult: true,
+        gender: "male",
+    },
+    {
+        name: "April",
+        location: "California",
+        isAdult: true,
+        gender: "female",
+    },
+    {
+        name: "Jessica",
+        location: "California",
+        isAdult: false,
+        grade: 4,
+        gender: "female",
+    },
+]
+
+// Write a function to print out
+//      Father
+//      Mother
+//      Child and school grade
+//
+// Example output:
+// Father is "Brian"
+// Mother is "April"
+// Child is "Jessica" in grade 4
+
