fa4b00eb04c7b94585e490a3cacc0bbcfc3ed8a1
Brian Hu <brianwhu@gmail.com>
2020-10-03 14:18:08 -0400

    update

--- homework/homework4.js
+++ homework/homework4.js
@@ -33,3 +33,16 @@ let merge = function(sorted1, sorted2) {
 
     return result;
 }
+
+/////////////////////////////////////////////////////
+
+let data = Array.from({length: 8}, () => Math.floor(Math.random() * 128));
+console.log(`Before reversing, data = ${data}`);
+let result = reverse(data);
+console.log(`After reversing, data = ${result}`);
+
+let array1 = Array.from({length: Math.floor(Math.random() * 10) + 4}, () => Math.floor(Math.random() * 128)).sort((a, b) => a - b);
+let array2 = Array.from({length: Math.floor(Math.random() * 10) + 4}, () => Math.floor(Math.random() * 128)).sort((a, b) => a - b);
+console.log(`Before merging, arries are\n${array1}\nand\n${array2}`);
+let merged = merge(array1, array2);
+console.log(`After merging, result = ${merged}`);
