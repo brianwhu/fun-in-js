ae9ceb050555fae753ba575996b6a6b4e5e9d2df
Brian Hu <brianwhu@gmail.com>
2020-10-17 14:10:00 -0400

    homework7

---
+++ homework/homework7.js
@@ -0,0 +1,35 @@
+
+/**
+ * Prints a solution to the tower of hanoi problem, i.e. moving all disks from the first pole to the third pole.
+ * 
+ * Hint: use recursion!
+ *
+ * Example 1:
+ *
+ *  hanoi('A', 'B', 'C', 1)
+ * 
+ * Prints
+ *  A -> C
+ *
+ * Example 2:
+ * 
+ *  hanoi('A', 'B', 'C', 2)
+ * 
+ * Prints
+ *  A -> B
+ *  A -> C
+ *  B -> C
+ * 
+ * @param a - the name of the first (starting) pole
+ * @param b - the name of the second pole
+ * @param c - the name of the third (ending) pole
+ * @param disks - the number of disks, which is at least 1
+ */
+let hanoi = function(a, b, c, disks) {
+}
+
+
+/////////////////////////////////////////////////////
+
+hanoi('A', 'B', 'C', 4);
+
