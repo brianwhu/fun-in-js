52dc2bda864f9df57bd9ad9025c135a890a74006
Brian Hu <brianwhu@gmail.com>
2020-10-31 14:46:23 -0400

    update

--- homework/homework8.js
+++ homework/homework8.js
@@ -1,4 +1,3 @@
-
 /**
  * Prints a paragraph so that all words fit in a column with the given width.
  * 
@@ -11,7 +10,7 @@
  * terms of n and the golden ratio, and
  * implies that the ratio of two consecutive
  * Fibonacci numbers tends to the golden
- * ratio as n increases
+ * ratio as n increases.
  * |=======================================|
  * 
  * Hint: use String.split() to help you.
@@ -20,6 +19,19 @@
  * @param width - the width of the column
  */
 let printColumn = function(text, width) {
+    let words = text.split(` `)
+    let line = "";
+
+    for (let i = 0; i < words.length; ++i) {
+        if (words[i].length + line.length <= width) {
+            line = line + words[i] + " ";
+        } else {
+            console.log(line.trimEnd());
+            line = words[i] + " ";
+        }
+    }
+
+    console.log(line);
 
 }
 
@@ -27,8 +39,9 @@ let printColumn = function(text, width) {
 /////////////////////////////////////////////////////
 let paragraph = `Fibonacci numbers are strongly related to the golden ratio: Binet's formula expresses the nth Fibonacci number in terms of n and the golden ratio, and implies that the ratio of two consecutive Fibonacci numbers tends to the golden ratio as n increases.`;
 
-const width = 50;
+const width = 52;
 
 console.log("=".repeat(width));
 printColumn(paragraph, width);
 console.log("=".repeat(width));
+
---
+++ homework/homework9.js
@@ -0,0 +1,39 @@
+
+/**
+ * Prints a tic tac toe grid
+ * 
+ * Example: Note the use of + at the corners and intersections.
+ * 
+ *  |-- 8 -|
+ * +--------------------------+
+ * |        |        |        | --
+ * |        |        |        |  3
+ * |        |        |        | --
+ * |--------+--------+--------|
+ * |        |        |        |
+ * |        |        |        |
+ * |        |        |        |
+ * |--------+--------+--------|
+ * |        |        |        |
+ * |        |        |        |
+ * |        |        |        |
+ * +--------------------------+
+ * 
+ * Hint:
+ *  String.repeat(number)
+ *  
+ * @param width - the width of a grid not including the lines
+ * @param height - the height of a grid not including the lines
+ */
+let printTicTacToeGrid = function(width, height) {
+
+}
+
+
+/////////////////////////////////////////////////////
+const width = 8;
+const height = 3;
+
+console.log(".".repeat(4 + 3*width));
+printTicTacToeGrid(width, height);
+
