decc45d9dc358a2e461f8891f833472db404dbb9
Brian Hu <brianwhu@gmail.com>
2021-03-03 20:01:47 -0500

    update

---
+++ projects/tetris_modules/PieceFactory.js
@@ -0,0 +1,21 @@
+import { J, L, T, S, Z, O, I } from "./Pieces.js";
+
+class PirceFactory {
+    /**
+     * Constructs a PieceFactory. This constructor does not need any arguments.
+     */
+    constructor() {
+
+    }
+
+    /**
+     * Return a random Tetris piece, i.e. one of J, L, T, S, Z, O, and I.
+     * 
+     * The randomness is produced by Math.random (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+     * 
+     * @returns a Piece
+     */
+    make() {
+        return null;
+    }
+}
