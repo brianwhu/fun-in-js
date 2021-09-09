5056020670df9e1acde1de52637813c789b5dc31
Jessica <jessica@macbook-pro.lan>
2021-09-09 16:45:43 -0700

    i finished tic tac toe

--- homework/tictactoe.js
+++ homework/tictactoe.js
@@ -35,8 +35,163 @@ import { readLines } from "https://deno.land/std/io/mod.ts";
     It is a tie!
 
 */
+let lower2number = s => s.charCodeAt(0) - 'a'.charCodeAt(0);
+
+//cell positions based on index
+let positions = [
+    {row: 0, column: 0},
+    {row: 0, column: 1},
+    {row: 0, column: 2},
+    {row: 1, column: 0},
+    {row: 1, column: 1},
+    {row: 1, column: 2},
+    {row: 2, column: 0},
+    {row: 2, column: 1},
+    {row: 2, column: 2},
+]
+
+let board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+]
+
+const labels = [
+    ' a   b   c ',
+    ' d   e   f ',
+    ' g   h   i '
+]
+
+let firstPlayer = 'X'
+let player = 'X'
+let moves = 0
+
+let printBoard = function() {
+    for (let i = 0; i < board.length; ++i) {
+        // print out one line in board[i]
+        let line = ''
+        for (let j = 0; j < board[i].length; ++j) {
+            // print out one cell in board[i][j]
+            line = line + '[' + board[i][j] + ']' + ' '
+        }
+        console.log(line)
+        console.log(labels[i])
+    }
+}
+
+let declareWinner = function(message) {
+    board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+
+    if (firstPlayer === 'X') {
+        firstPlayer = 'O'
+        player = 'O'
+    } else {
+        firstPlayer = 'X'
+        player = 'X'
+    }
+    
+    console.log('')
+    console.log(message)
+    console.log('')
+    console.log('NEW GAME')
+    printBoard()
+    console.log(`Player '${player}', please enter your move:`)
+}
+
+printBoard()
 
 const input = readLines(Deno.stdin);
+console.log(`Player '${player}', please enter your move:`)
 for (let data = await input.next(); !data.done; data = await input.next()) {
-    console.log(data.value);
+    //change board
+    //1. validate input.value
+    //2. if invalid, report error, then continue
+    //3. if valid, check that the cell is open, then change the board
+    //4. otherwise, report error
+
+    if (data.value.length !== 1) {
+        console.log('Please enter a single letter.')
+        continue
+    }
+    if (data.value < 'a' || data.value > 'i') {
+        console.log('Please enter a single letter between the letters "a" and "i".')
+        continue
+    }
+
+    let cellPosition = positions[lower2number(data.value)]
+    if (board[cellPosition.row][cellPosition.column] === ' ') {
+        board[cellPosition.row][cellPosition.column] = `${player}`
+    } else {
+        console.log ('This cell is taken. Please try another cell.')
+        continue
+    }
+    ++moves
+
+    //print board
+    printBoard()
+    
+    //decide if someone won
+    let count = 0
+    let gameOver = false
+    for (let i = 0; i < 3; ++i) {
+        if (board[i][cellPosition.column] === player) {
+            ++count
+            gameOver = count === 3
+        } else {
+            count = 0
+            break
+        }
+    }
+    if (!gameOver) {
+        for (let i = 0; i < 3; ++i) {
+            if (board[cellPosition.row][i] === player) {
+                ++count
+                gameOver = count === 3
+            } else {
+                count = 0
+                break
+            }
+        }
+    }
+    if (!gameOver) {
+        for (let i = 0; i < 3; ++i) {
+            if (board[i][i] === player) {
+                ++count
+                gameOver = count === 3
+            } else {
+                count = 0
+                break
+            }
+        }
+    }
+    if (!gameOver) {
+        for (let i = 0; i < 3; ++i) {
+            if (board[i][2 - i] === player) {
+                ++count
+                gameOver = count === 3
+            } else {
+                count = 0
+                break
+            }
+        }
+    }
+    
+    if (gameOver) {
+        declareWinner(`Player '${player}' wins!`)
+    } else if (moves === 9) {
+        declareWinner('It is a tie!')
+    } else {
+        //switch player
+        if (player === 'X') {
+            player = 'O'
+        } else {
+            player = 'X'
+        }
+        //prompt player
+        console.log(`Player '${player}', please enter your move:`)
+    }
 }
