5c9dbf22d964683eab7fa876c6a9380a0192edbf
Jessica <jessica@macbook-pro.lan>
2021-09-16 17:32:40 -0700

    done

--- homework/HiddenPhrase.js
+++ homework/HiddenPhrase.js
@@ -94,11 +94,67 @@ let nouns = [
   'Fact',
 ];
 
+let player = 'X'
+let switchPlayer = function() {
+    if (player === 'X') {
+        player = 'O'
+    } else {
+        player = 'X'
+    }
+}
+
 // generate a random secret phrase
+let actualAdjective
+let actualNoun
+let phrase
+let generatePhrase = function() {
+    actualAdjective = `${adjectives[Math.floor(Math.random() * adjectives.length)]}`
+    actualNoun = `${nouns[Math.floor(Math.random() * nouns.length)]}`
+    phrase = `${actualAdjective} ${actualNoun}`
+}
 
 // display the phrase using placeholders: .... ....
+let placeholder
+let generatePlaceholder = function() {
+    placeholder = `${'.'.repeat(actualAdjective.length)} ${'.'.repeat(actualNoun.length)}`
+}
+
+let isSolutionCorrect = function(solution) {
+    // return true if solution is correct, false otherwise
+    return solution.toLowerCase().replaceAll(' ', '') === phrase.toLowerCase().replaceAll(' ', '')
+}
+
+let gameOver = function() {
+    console.log('')
+    console.log(`Player '${player}' wins! The phrase was ${phrase}.`)
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('NEW GAME')
+    switchPlayer()
+    generatePhrase()
+    generatePlaceholder()
+    console.log(placeholder)
+    console.log(`Player '${player}', please enter your guess (1 letter or SOLVE)`)
+}
+
+let gameplay = function(letter) {
+    // test
+    let lowerCasePhrase = phrase.toLowerCase()
+    let placeholderArray = placeholder.split('')
+    for (let i = 0; i < lowerCasePhrase.length; ++i) {
+        if (letter === lowerCasePhrase.charAt(i)) {
+            placeholderArray[i] = phrase.charAt(i)
+        }
+    }
+    placeholder = placeholderArray.join('')
+}
 
 // prompt
+generatePhrase()
+generatePlaceholder()
+console.log(placeholder)
+console.log(`Player '${player}', please enter your guess (1 letter or SOLVE)`)
 
 const input = readLines(Deno.stdin);
 for (let data = await input.next(); !data.done; data = await input.next()) {
@@ -109,15 +165,42 @@ for (let data = await input.next(); !data.done; data = await input.next()) {
     //          this player wins
     //      otherwise
     //          the opponent wins
+    if (data.value === 'SOLVE') {
+        console.log(`Player '${player}', please enter your solution`)
+        data = await input.next()
+        if (isSolutionCorrect(data.value)) {
+            gameOver()
+        } else {
+            switchPlayer()
+            gameOver()
+            continue
+        }
     // otherwise
     //      check to make sure the input is 1 letter between 'a' to 'z'
     //      test to see if this is a correct letter
     //      update phrase placeholder
+    } else if (data.value.length !== 1) {
+        console.log('Please enter a single letter.')
+        continue
+    } else if (data.value < 'a' || data.value > 'z') {
+        console.log('Please enter a single letter between the letters "a" and "z".')
+        continue
+    } else {
+        gameplay(data.value)
+    }
+
+    if (placeholder === phrase) {
+        gameOver()
+        continue
+    }
 
     // display the phrase using placeholders: .... ....
+    console.log(placeholder)
 
     // switch player
+    switchPlayer()
 
     // prompt
+    console.log(`Player '${player}', please enter your guess (1 letter or SOLVE)`)
 }
 
