435ba1b5054b14234b0d5d353c7522ede917f670
Jessica <jessica@macbook-pro.lan>
2022-06-19 11:26:59 -0700

    i finished homework

--- homework/ObjectPrinter.js
+++ homework/ObjectPrinter.js
@@ -14,6 +14,50 @@ class ObjectPrinter {
         // print YAML document mark
         console.log("---");
         // continue with object printing
+        let indent = ""
+        console.log(ObjectPrinter.printUnknown(indent, data))
+    }
+
+    static printUnknown(indent, data) {
+        if (data.constructor === Object) {
+            return ObjectPrinter.printObject(indent, data, false)
+        } else if (data.constructor === Array) {
+            return ObjectPrinter.printArray(indent, data)
+        } else {
+            return data.toString()
+        }
+    }
+
+    static printObject(indent, data, insideArray) {
+        let output = ""
+        let names = Object.keys(data)
+        for (let i = 0; i < names.length; ++i) {
+            let value = data[names[i]]
+            let realIndent = i === 0 && insideArray ? '' : indent
+            if (value.constructor === Object) {
+                output += realIndent + names[i] + ":\n" + this.printObject(indent + "  ", value, false)
+            } else if (value.constructor === Array) {
+                output += realIndent + names[i] + ":\n" + this.printArray(indent, value)
+            } else {
+                output += realIndent + names[i] + ": " + value + "\n"
+            }
+        }
+        return output
+    }
+
+    static printArray(indent, data) {
+        let output = ""
+        for (let i = 0; i < data.length; ++i) {
+            let value = data[i]
+            if (value.constructor === Object) {
+                output += indent + "- " + this.printObject(indent + "  ", value, true)
+            } else if (value.constructor === Array) {
+                output += indent + "-\n" + this.printArray(indent, value)
+            } else {
+                output += indent + "- " + value + "\n"
+            }
+        }
+        return output
     }
 }
 
--- test/ObjectPrinterTest.js
+++ test/ObjectPrinterTest.js
@@ -2,16 +2,13 @@
 
 import { ObjectPrinter } from "../homework/ObjectPrinter.js"
 
-if (Deno.args.length > 0) {
-    Deno.args.forEach(argument => {
-        try {
-            let data = JSON.parse(Deno.readTextFileSync(argument));
-            ObjectPrinter.yaml(data);
-        } catch (x) {
-            console.error(`*** ${argument}: ${x.name}`);
-            return
-        }
-    })
-} else {
-    console.error("Usage: ObjectPrinterTest <json-file> ...");
-}
+Deno.args.forEach(argument => {
+    try {
+        let data = JSON.parse(Deno.readTextFileSync(argument));
+        ObjectPrinter.yaml(data);
+    } catch (x) {
+        console.error(`*** ${argument}: ${x.name}`);
+        throw x
+    }
+})
+
