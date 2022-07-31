553e11f408a03192e4ed39d0594079c536b1207d
Jessica <jessica@macbook-pro.lan>
2022-07-31 11:14:57 -0700

    Please enter the commit message for your changes. Lines starting

--- canvas/projects/curve.js
+++ canvas/projects/curve.js
@@ -1,10 +1,11 @@
 import ComplexPlane from "../modules/ComplexPlane.js"
 import DrawingBoard from "../modules/DrawingBoard.js"
 
-const plane = new ComplexPlane(-10, 10, 0)
+const plane = new ComplexPlane(-10, 10, -5)
 const board = new DrawingBoard(plane)
+const A = 0.5
 
-let quadratic = x => x*x
+let quadratic = x => A*x*x
 
 board.save()
 
@@ -26,7 +27,14 @@ for (let i = 0; i < points.length - 1; ++i) {
 }
 */
 
-let xNumbers = [ -2, -1, 0, 1, 2 ]
+// let xNumbers = [ -2, -1.875, -1.75, -1.625, -1.5, -1.375, -1.25, -1.125, -1, -0.875, -0.75, -0.625, -0.5, -0.375, -0.25, -0.125, 0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1, 1.125, 1.25, 1.375, 1.5, 1.625, 1.75, 1.875, 2 ]
+const N = A*50
+const Increment = 0.075/A
+let xNumbers = []
+for (let i = 0; i < N; ++i) {
+    xNumbers.unshift(-i * Increment)
+    xNumbers.push(i * Increment)
+}
 
 for (let i = 0; i < xNumbers.length - 1; ++i) {
     let x1 = xNumbers[i];
@@ -35,7 +43,35 @@ for (let i = 0; i < xNumbers.length - 1; ++i) {
     let x2 = xNumbers[i+1];
     let y2 = quadratic(x2)
 
-    board.line({ x: plane.x(x1), y: plane.y(y1) }, { x: plane.x(x2), y: plane.y(y2) }, 2, "red")
+    board.line({ x: plane.x(x1), y: plane.y(y1) }, { x: plane.x(x2), y: plane.y(y2) }, 2, "dodgerblue")
 }
     
 board.restore()
+
+function draw() {
+    const canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
+
+        var grd = ctx.createLinearGradient(425, 425, 600, 600)
+        grd.addColorStop(0, '#FA5858')
+        grd.addColorStop(0.08, '#FAAC58')
+        grd.addColorStop(0.16, '#F4FA58')
+        grd.addColorStop(0.24, '#ACFA58')
+        grd.addColorStop(0.32, '#58FA58')
+        grd.addColorStop(0.4, '#58FAAC')
+        grd.addColorStop(0.48, '#58FAF4')
+        grd.addColorStop(0.56, '#58ACFA')
+        grd.addColorStop(0.64, '#5858FA')
+        grd.addColorStop(0.72, '#AC58FA')
+        grd.addColorStop(0.8, '#FA58F4')
+        grd.addColorStop(0.88, '#FA58AC')
+
+        ctx.beginPath()
+        ctx.fillStyle = grd
+        ctx.arc(500, 500, 100, 0, Math.PI * 2, true)
+        ctx.fill()
+    }
+  }
+
+draw()
